3e35f8da4cc52a30cbe305419015ba6e56824543
Ahmad Baig Shaik <ab18145@corp.ojas-it.com>
2021-12-07 12:25:08 +0530

    Hello world

---
+++ bin/enable-sl1-admin.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import json
+import boto3
+
+parameter1 = os.environ['parameter1']
+parameter2 = os.environ['parameter2']
+
+def main():
+    # Parameters :: instanceid,region_name
+    instanceid = "i-063de1af7ebc39fb7"
+    print("Hello ", parameter1)
+    print("Hiiii ", parameter2)
+##    ssm_client = boto3.client("ssm", region_name="ap-south-1",)
+##    responce = ssm_client.send_command(InstanceIds=[
+##                                       instanceid], DocumentName="AWS-RunShellScript",
+##                                       Parameters={"commands": ["hostname"]})
+##    command_id = responce["Command"]["CommandId"]
+##    output = ssm.client.get_command_invocation(
+##        CommandId=command_id,
+##        InstanceId=instanceid
+##    )
+##    while output["Status"] == "InProgress":
+##        output = ssm.client.get_command_invocation(
+##            CommandId=command_id,
+##            InstanceId=instanceid
+##        )
+##    return output["StandardOutputContent"]
+
+
+if __name__ == "__main__":
+    main()
