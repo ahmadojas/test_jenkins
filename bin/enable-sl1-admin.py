4ab1ce60de7397dc2bd1a412a20c11aa26e8a33d
Ahmad Baig Shaik <ab18145@corp.ojas-it.com>
2022-01-10 12:35:11 +0530

    testing file creation

--- bin/enable-sl1-admin.py
+++ bin/enable-sl1-admin.py
@@ -3,32 +3,71 @@ import os
 import json
 import boto3
 import time
+import logging
+import sys
+import traceback
 
-INSTANCE_ID = os.environ['INSTANCE_ID']
-ACCOUNT_ID = os.environ['AWS_ACCOUNT_ID']
-AWS_REGION = os.environ['AWS_REGION']
-ACTION = os.environ['ACTION']
-DURATION = os.environ['DURATION']
-Name = os.getenv
-Name1 = os.environ['BUILD_TRIGGER_BY']
+print("hsgshg", sys.argv)
+enabled_file_name = 'sl1_users_enabled.log'
+suspended_file_name = 'sl1_users_suspended.log'
 
-def main():
+logging.basicConfig(level=logging.DEBUG, filename=enabled_file_name, format='%(asctime)s %(levelname)s: %(message)s')
+logger1 = logging.getLogger()
+# print("handlers", logger1.handlers)
+first = logger1.handlers[0]
+print("first handler", first)
+
+def main(instance, account, region, action, duration):
     # Parameters :: instanceid,region_name
     instanceid = "i-063de1af7ebc39fb7"
-##    time.sleep(5)
     print("This is ", INSTANCE_ID)
-##    time.sleep(5)
+
     print("Wishing you ", ACCOUNT_ID)
     print("Wishing you a very ", Name)
     print("Wishing you a very beutiful ", Name1)
-    with open("test_file.txt", "w+") as f:
-        print("Writing into the file")
-        f.write("Hey Triggered Man {}".format(Name1))
-        # print("File created")
-    with open("test_file.txt", "r") as fr:
-        print("Reading from file")
-        print("Lines wriiten in the file are : \n", fr.read())   
+##    with open("test_file.txt", "w+") as f:
+##        print("Writing into the file")
+##        f.write("Hey Triggered Man {}".format(Name1))
+##        # print("File created")
+##    with open("test_file.txt", "r") as fr:
+##        print("Reading from file")
+##        print("Lines wriiten in the file are : \n", fr.read())   
     print(INSTANCE_ID, ACCOUNT_ID, AWS_REGION, ACTION, DURATION)
+    try:
+        #"hi" + 2
+        data = {"instance_id": instance, "aws_account_id": account, "aws_region": region, "action": action, "duration": duration}
+        if action == 'activate':
+            data1 = ", ".join(data.values())
+            print(data1)
+            logging.info(data1)
+        elif action == 'suspend':
+            f = open(enabled_file_name, 'r')
+            data['action'] = 'activate'
+            data1 = ", ".join(data.values())
+            print("data1", data1)
+            lines = f.readlines()
+            print("lines", lines)
+            f.close()
+            f1 = open(enabled_file_name, 'w+')
+            for line in lines:
+                if data1 not in line:
+                    print("line", line)
+                    f1.write(line)
+                    
+                else:
+                    logger1.removeHandler(first)
+                    logging.basicConfig(level=logging.DEBUG, filename=suspended_file_name, format='%(asctime)s %(levelname)s: %(message)s')
+                    #logger1.removeHandler(first)
+                    print("handlers1111111", logger1.handlers)
+                    #logger = logging.getLogger()
+                    data['action'] = 'suspend'
+                    data2 = ", ".join(data.values())
+                    print("Deleting... ", data1)
+                    logging.info(data2)
+        else:
+            print('Please pass valid command as action')
+    except Exception as e:
+        logger1.exception(e)
 ##    time.sleep(5)
 ##    ssm_client = boto3.client("ssm", region_name="ap-south-1",)
 ##    responce = ssm_client.send_command(InstanceIds=[
@@ -48,4 +87,22 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+##    instance = sys.argv[1]  # 'i-hgsdh223b' 
+##    account =  sys.argv[2]  # '12345678'
+##    region =   sys.argv[3]  # 'ap-south-2'
+##    action =   sys.argv[4]  # 'suspend'
+##    # duration = '2'
+##    if len(sys.argv) > 5:
+##        duration = sys.argv[5]  # sys.argv[5]
+##    else:
+##        duration = None
+##
+##    create_log(instance, account, region, action, duration)
+    INSTANCE_ID = os.environ['INSTANCE_ID']
+    ACCOUNT_ID = os.environ['AWS_ACCOUNT_ID']
+    AWS_REGION = os.environ['AWS_REGION']
+    ACTION = os.environ['ACTION']
+    DURATION = os.environ['DURATION']
+    Name = os.getenv
+    Name1 = os.environ['BUILD_TRIGGER_BY']
+    main(INSTANCE_ID, ACCOUNT_ID, AWS_REGION, ACTION, DURATION)
