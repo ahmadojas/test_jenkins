9cf852a65cc48a132928e55db953cd334f2a17d4
Ahmad Baig Shaik <ab18145@corp.ojas-it.com>
2022-01-10 11:10:36 +0530

    testing file creation

--- bin/enable-sl1-admin.py
+++ bin/enable-sl1-admin.py
@@ -21,7 +21,7 @@ def main():
     print("Wishing you ", ACCOUNT_ID)
     print("Wishing you a very ", Name)
     print("Wishing you a very beutiful ", Name1)
-    with opne("test_file.txt", "w+"):
+    with open("test_file.txt", "w+"):
         print("File created")
     print(INSTANCE_ID, ACCOUNT_ID, AWS_REGION, ACTION, DURATION)
 ##    time.sleep(5)
